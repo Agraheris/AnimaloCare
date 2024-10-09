e61a84a6a333dcc64a5603c9d0b9211c00f47a0a
clement Vigouroux <clement.vigouroux@gmail.com>
2024-10-09 12:12:54 +0200

    creation and Style NavBar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,18 @@
+import Logo1 from "../assets/images/Logo 1.png";
+
+import "../assets/style/Navbar.css";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <nav>
+      <img src={Logo1} alt="logo AnimaloCare" />
+      <h1>Animalocare</h1>
+      <section className="navButton">
+        <button type="button"> Inscription </button>
+        <button type="button"> Connection </button>
+      </section>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
